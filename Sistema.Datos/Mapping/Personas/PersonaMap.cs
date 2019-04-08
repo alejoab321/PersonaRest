df92912bddd088375d83e39709bda01efaa2998d
alejoab321 <alejoab1020@gmail.com>
2019-04-08 16:51:28 -0500

    Web Api utilizando mappers

---
+++ Sistema.Datos/Mapping/Personas/PersonaMap.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Collections.Generic;
+using System.Text;
+using Sistema.Entidades.Personas;
+
+namespace Sistema.Datos.Mapping.Personas
+{
+    public class PersonaMap : IEntityTypeConfiguration<Persona>
+    {
+        public void Configure(EntityTypeBuilder<Persona> builder)
+        {
+            builder.ToTable("Personas")
+                .HasKey(p => p.IdPersona);
+            builder.Property(p => p.Apellidos)
+                .HasMaxLength(50);
+            builder.Property(P => P.Nombre)
+                .HasMaxLength(50);
+        }
+    }
+}
